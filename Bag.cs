3d1504f08d077b07fb2f7e1adc21df726d0f7c8d
Colan Biemer <colan@comcast.net>
2018-01-08 15:34:49 -0800

    added comments to bag.cs

--- Bag.cs
+++ Bag.cs
@@ -2,18 +2,27 @@
 
 namespace BGCTools
 {
-    // @note: this class is okay but not perfect. My particular issue with it is 
-    //        that I cannot clone the templates given. Cloning the templates 
-    //        would be ideal because we can keep the clean list safe from outside
-    //        damage through modification. Inherently the code is less safe with
-    //        this data structure if the programmer is not paying attention and 
-    //        making sure to program in cloning after a Pull and also not modifying
-    //        the passed in list.
+    /// <summary>
+    /// Bag implemnetation: https://algs4.cs.princeton.edu/13stacks/
+    /// 
+    /// @note: this class is okay but not perfect. My particular issue with it is 
+    //         that I cannot clone the templates given. Cloning the templates 
+    //         would be ideal because we can keep the clean list safe from outside
+    //         damage through modification. Inherently the code is less safe with
+    //         this data structure if the programmer is not paying attention and 
+    //         making sure to program in cloning after a Pull and also not modifying
+    //         the passed in list.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Bag<T>
     {
         private List<T> cleanList = new List<T>();
         private List<T> bag       = new List<T>();
 
+        /// <summary>
+        /// Constructor method for bag with a list to build from
+        /// </summary>
+        /// <param name="startingCleanList"></param>
         public Bag(List<T> startingCleanList)
         {
             for (int i = 0; i < startingCleanList.Count; ++i)
@@ -24,22 +33,52 @@ namespace BGCTools
             Reset();
         }
 
+        /// <summary>
+        /// Constructor method for a bag with an array to build from
+        /// </summary>
+        /// <param name="startingCleanList"></param>
+        public Bag(T[] startingCleanList)
+        {
+            for (int i = 0; i < startingCleanList.Length; ++i)
+            {
+                cleanList.Add(startingCleanList[i]);
+            }
+
+            Reset();
+        }
+
+        /// <summary>
+        /// Add element to the bag
+        /// </summary>
+        /// <param name="element"></param>
         public void Add(T element)
         {
             cleanList.Add(element);
         }
 
+        /// <summary>
+        /// add element to clean list and current bag
+        /// </summary>
+        /// <param name="element"></param>
         public void AddToBoth(T element)
         {
             cleanList.Add(element);
             bag.Add(element);
         }
 
+        /// <summary>
+        /// Rese tbag to have entire clean list
+        /// </summary>
         public void Reset()
         {
             bag = new List<T>(cleanList);
         }
 
+        /// <summary>
+        /// Pull from the bag and reset if autoreset is set to true else receive default value.
+        /// </summary>
+        /// <param name="autoReset"></param>
+        /// <returns></returns>
         public T Pull(bool autoReset = false)
         {
             T element;
@@ -68,6 +107,10 @@ namespace BGCTools
             return element;
         }
 
+        /// <summary>
+        /// Returns whether or not the bag is empty
+        /// </summary>
+        /// <returns></returns>
         public bool IsEmpty()
         {
             return bag.Count <= 0;
