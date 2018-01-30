fef2ff8a9e6a494fcb8aa48d56efcbb49d4deb6d
Colan Biemer <colan@comcast.net>
2018-01-29 16:10:12 -0800

    added mod to general utility

---
+++ Utility/Math/GeneralMath.cs
@@ -0,0 +1,29 @@
+﻿
+namespace BGC.Utility.Math
+{
+    public static class GeneralMath
+    {
+        /// <summary>
+        /// x mod m
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static int Mod(int x, int m)
+        {
+            // https://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain
+            return ((x % m) + m) % m;
+        }
+
+        /// <summary>
+        /// x mood m
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static float Mod(float x, float m)
+        {
+            return ((x % m) + m) % m;
+        }
+}
+}
