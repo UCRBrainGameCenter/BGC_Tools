c635a7c42b68c9d357c23df3b2562954db422877
Colan Biemer <colan@comcast.net>
2018-03-05 05:33:27 -0800

    added conversions for seconds to micro seconds and vise versa

---
+++ Utility/Math/Conversion.cs
@@ -0,0 +1,22 @@
+﻿public static class Conversion
+{
+    public static float SecondsToMS(float seconds)
+    {
+        return seconds * 1000f;
+    }
+
+    public static float MSToSeconds(float ms)
+    {
+        return ms * 0.001f;
+    }
+
+    public static double SecondsToMS(double seconds)
+    {
+        return seconds * 1000;
+    }
+
+    public static double MSToSeconds(double ms)
+    {
+        return ms * 0.001;
+    }
+}
