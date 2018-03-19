6e61f6a199a7b49e61ba600e3d7c6d07ac900642
Lorenzo Alamillo <lalam003@ucr.edu>
2018-03-19 14:07:03 -0700

    Converts Lists to Json Arrays
    Converts Lists to Json Arrays

---
+++ Extensions/JsonExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using LightJson;
+
+namespace BGC.Extensions
+{
+    public static class JsonExtensions
+    {
+        /// <summary>
+        /// Converts an int list to a Json Array of Int Values
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static JsonArray IntListToJsonArray(this List<int> list)
+        {
+            JsonArray arr = new JsonArray();
+            for(int i = 0; i < list.Count; i++)
+            {
+                arr.Add(list[i]);
+            }
+            return arr;
+        }
+
+        /// <summary>
+        /// Converts any list to a Json Array of String Values
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static JsonArray AnyListToJsonArray<T>(this List<T> list)
+        {
+            JsonArray arr = new JsonArray();
+            for(int i = 0; i < list.Count; i++)
+            {
+                arr.Add(list[i].ToString());
+            }
+            return arr;
+        }
+    }
+}
