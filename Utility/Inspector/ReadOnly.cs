f0e53d24aade772ea4609b7f89b907ca9fbf0735
Colan Biemer <colan@comcast.net>
2018-03-19 14:45:22 -0700

    read only

---
+++ Utility/Inspector/ReadOnly.cs
@@ -0,0 +1,37 @@
+﻿// modified from: https://answers.unity.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace BGC.Utility.Inspector
+{
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(
+            SerializedProperty property,
+            GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(
+            Rect position,
+            SerializedProperty property,
+            GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+#endif
+}
