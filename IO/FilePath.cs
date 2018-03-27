4f8f3e065350a74a9efddb59dd93c2cc252cfe58
Colan Biemer <colan@comcast.net>
2018-03-27 17:48:17 -0500

    addded path, json file reading, and parsing excpetions

---
+++ IO/FilePath.cs
@@ -0,0 +1,21 @@
+﻿namespace BGC.IO
+{
+    public static class FilePath
+    {
+        /// <summary>
+        /// Replace all invalid characters for a file with an empty value unless you 
+        /// specificy otherwise
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="replacementValue"></param>
+        public static void ReplaceInvalidCharacters(ref string path, string replacementValue="")
+        {
+            char[] invalidCharacters = System.IO.Path.GetInvalidFileNameChars();
+
+            for (int i = 0; i < invalidCharacters.Length; ++i)
+            {
+                path = path.Replace(invalidCharacters[i].ToString(), replacementValue);
+            }
+        }
+    }
+}
