5faf23b00d269104165138054d048e9358e54921
Lorenzo Alamillo <lalam003@ucr.edu>
2018-05-04 12:15:02 -0700

    Added WorldToScreen Conversion (#6)
    * Added WorldToScreen Conversion
    
    * Added Namespace BGC.Utility.Math

--- Utility/Math/Conversion.cs
+++ Utility/Math/Conversion.cs
@@ -1,22 +1,36 @@
-﻿public static class Conversion
+﻿using UnityEngine;
+
+namespace BGC.Utility.Math
 {
-    public static float SecondsToMS(float seconds)
+    public static class Conversion
     {
-        return seconds * 1000f;
-    }
+        public static float SecondsToMS(float seconds)
+        {
+            return seconds * 1000f;
+        }
 
-    public static float MSToSeconds(float ms)
-    {
-        return ms * 0.001f;
-    }
+        public static float MSToSeconds(float ms)
+        {
+            return ms * 0.001f;
+        }
 
-    public static double SecondsToMS(double seconds)
-    {
-        return seconds * 1000;
-    }
+        public static double SecondsToMS(double seconds)
+        {
+            return seconds * 1000;
+        }
 
-    public static double MSToSeconds(double ms)
-    {
-        return ms * 0.001;
+        public static double MSToSeconds(double ms)
+        {
+            return ms * 0.001;
+        }
+
+        public static Vector2 WorldToScreen(Vector3 worldPos, RectTransform screenRect)
+        {
+            Vector2 localPoint;
+            Vector3 screenPoint = Camera.main.WorldToScreenPoint(worldPos);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(screenRect, screenPoint, Camera.main, out localPoint);
+
+            return localPoint;
+        }
     }
 }
