305ec36d76a3f7dd454e6ec4ea6da016ee0b6e06
TStavrop <Trevor.Stavropoulos@gmail.com>
2018-05-09 09:47:55 -0700

    Adding ByteMath

---
+++ Utility/Math/ByteMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace BGC.Utility.Math
+{
+    public static class ByteMath
+    {
+        public static byte RoundClamp(float value) =>
+            (byte)Mathf.Clamp(Mathf.RoundToInt(value), byte.MinValue, byte.MaxValue);
+        public static byte FloorClamp(float value) =>
+            (byte)Mathf.Clamp(Mathf.FloorToInt(value), byte.MinValue, byte.MaxValue);
+
+        public static byte RoundRepeat(float value) => (byte)Mathf.RoundToInt(value);
+        public static byte FloorRepeat(float value) => (byte)Mathf.FloorToInt(value);
+    }
+}
