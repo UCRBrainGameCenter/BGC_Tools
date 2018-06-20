5e43971affc716f3010cba3c9f1c186e61a05351
nadnbuds <lalam003@ucr.edu>
2018-06-20 10:35:49 -0700

    Added tests for BGC_Tools

---
+++ Editor/Tests/BagTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using BGC.DataStructures.Generic;
+
+public class BagTests
+{
+    private List<int> sampleList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    [Test]
+    public void PsEmptyTests()
+    {
+        DepletableBag<int> bag = new DepletableBag<int>(sampleList);
+        Assert.IsFalse(bag.Count <= 0);
+
+        for (int i = 0; i < sampleList.Count - 1; ++i)
+        {
+            bag.PopNext();
+            Assert.IsFalse(bag.Count <= 0);
+        }
+
+        bag.PopNext();
+        Assert.IsTrue(bag.Count <= 0);
+    }
+
+    [Test]
+    public void PullTests()
+    {
+        DepletableBag<int> bag = new DepletableBag<int>(sampleList);
+        Dictionary<int, bool> dictionary = new Dictionary<int, bool>();
+
+        for (int i = 0; i < sampleList.Count; ++i)
+        {
+            dictionary.Add(i, false);
+        }
+
+        while (bag.Count > 0)
+        {
+            dictionary[bag.PopNext()] = true;
+        }
+
+        for (int i = 0; i < sampleList.Count; ++i)
+        {
+            Assert.IsTrue(dictionary[i + 1]);
+        }
+
+        for (int i = 0; i < 100; ++i)
+        {
+            if (i >= sampleList.Count)
+            {
+                Assert.AreEqual(default(int), bag.PopNext());
+            }
+        }
+    }
+
+    [Test]
+    public void PullAutoResetTests()
+    {
+        DepletableBag<int> bag = new DepletableBag<int>(sampleList);
+        bag.AutoRefill = true;
+
+        for (int i = 0; i < 100; ++i)
+        {
+            Assert.AreNotEqual(default(int), bag.PopNext());
+        }
+    }
+}
---
+++ Editor/Tests/RandomValueTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using BGC.Extensions;
+using NUnit.Framework;
+
+public class RandomValueTests
+{
+    private List<int> sampleList = new List<int>() { 1, 2, 3};
+
+    [Test]
+    public void RVTestMaxIndicies()
+    {
+        int rv = sampleList.RandomValue<int>(0, 1, 2);
+        UnityEngine.TestTools.LogAssert.Expect(
+            UnityEngine.LogType.Error, 
+            "Recieved array of excludedIndicies that does not allow for any values to be returned, " +
+            "returning default value");
+        Assert.IsTrue(rv == default(int));
+    }
+
+    [Test]
+    public void RVTestOutOfRangeIndicies()
+    {
+        int rv = sampleList.RandomValue<int>(-1, 3);
+        Assert.IsTrue(rv > 0 && rv < 4);
+    }
+
+    [Test]
+    public void RVTestAllButOneIndicies()
+    {
+        int rv = sampleList.RandomValue<int>(0, 2);
+        Assert.IsTrue(rv == 2);
+    }
+}
