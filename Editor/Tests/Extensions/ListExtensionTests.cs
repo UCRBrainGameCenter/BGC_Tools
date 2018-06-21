fa19fa68dce22312beaf80f07d75c5c92b2fda18
nadnbuds <lalam003@ucr.edu>
2018-06-21 11:53:33 -0700

    Moved Tests

---
+++ Editor/Tests/Extensions/ListExtensionTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using BGC.Extensions;
+using NUnit.Framework;
+
+namespace BGC.Tests
+{
+    public class RandomValueTests
+    {
+        private readonly List<int> sampleList = new List<int>() { 1, 2, 3 };
+
+        [Test]
+        public void RVTestMaxIndicies()
+        {
+            int rv = sampleList.RandomValue<int>(0, 1, 2);
+            UnityEngine.TestTools.LogAssert.Expect(
+                UnityEngine.LogType.Error,
+                "Recieved array of excludedIndicies that does not allow for any values to be returned, " +
+                "returning default value");
+            Assert.IsTrue(rv == default(int));
+        }
+
+        [Test]
+        public void RVTestOutOfRangeIndicies()
+        {
+            int rv = sampleList.RandomValue<int>(-2, -1, 3, 4);
+            Assert.IsTrue(rv > 0 && rv < 4);
+        }
+
+        [Test]
+        public void RVTestAllButOneIndicies()
+        {
+            int rv = sampleList.RandomValue<int>(0, 2);
+            Assert.IsTrue(rv == 2);
+        }
+
+        [Test]
+        public void RVTestNoIndicies()
+        {
+            int rv = sampleList.RandomValue<int>();
+            Assert.IsTrue(rv > 0 && rv < 4);
+        }
+    }
+}
