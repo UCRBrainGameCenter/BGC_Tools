58f23e327c190bfb01e4245e83c8ec3a258ca1e9
nadnbuds <lalam003@ucr.edu>
2018-08-22 09:20:53 -0700

    TextField

---
+++ Utility/TextFieldPopup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace BGC.Utility
+{
+    public class TextPopupField : EditorWindow
+    {
+        /// <summary>
+        /// Ensures that the Popup is initialized
+        /// </summary>
+        private bool Initialized = false;
+
+        /// <summary>
+        /// Text to display
+        /// </summary>
+        private string text;
+
+        /// <summary>
+        /// Text that user will input
+        /// </summary>
+        private string inputText;
+
+        /// <summary>
+        /// Callback that sends input field
+        /// </summary>
+        private Action<string> callback;
+
+        /// <summary>
+        /// Initializes the PopupField
+        /// </summary>
+        public void Init(string text, Action<string> callback)
+        {
+            Initialized = true;
+
+            this.text = text;
+            this.callback = callback;
+        }
+
+        void OnGUI()
+        {
+            Assert.IsTrue(Initialized);
+
+            GUILayout.Label(text);
+
+            inputText = EditorGUILayout.TextField(inputText);
+
+            if (GUILayout.Button("Submit"))
+            {
+                callback(inputText);
+                this.Close();
+            }
+
+            if (GUILayout.Button("Close"))
+            {
+                this.Close();
+            }
+        }
+    }
+}
