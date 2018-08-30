e35f4dfa018aefc8fc8bf5b5b724920a430a3fdc
nadnbuds <lalam003@ucr.edu>
2018-08-30 10:03:34 -0700

    Added recusive option to getting files from path

--- IO/ResourceInfo.cs
+++ IO/ResourceInfo.cs
@@ -13,7 +13,7 @@ namespace BGC.IO
         private static List<string> resources;
         private static int length;
 
-        public static List<string> ListFiles(string path)
+        public static List<string> ListFiles(string path, bool recursive = false)
         {
             InitializeResourceInfo();
             List<string> paths = new List<string>();
@@ -34,7 +34,7 @@ namespace BGC.IO
                 if (filePath.Contains(path))
                 {
                     resourceDirectoryCount = filePath.Split(ResourceInfoConstructor.CharPathSeparator).Length;
-                    if (directoryCount == resourceDirectoryCount)
+                    if (directoryCount == resourceDirectoryCount || recursive)
                     {
                         paths.Add(filePath);
                     }
