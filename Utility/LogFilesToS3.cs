d7977713e39c602879a49b7f1df3410ac6fbb8ba
TStavrop <Trevor.Stavropoulos@gmail.com>
2018-10-31 14:40:59 -0700

    Changing pushing interface and adding repush option

--- Utility/LogFilesToS3.cs
+++ Utility/LogFilesToS3.cs
@@ -1,29 +1,34 @@
 ﻿using UnityEngine.Networking;
 using UnityEngine.Assertions;
 using UnityEngine;
+using System.Collections.Generic;
 using System.IO;
 using BGC.Web;
 using BGC.IO;
 
 namespace BGC.Utility
 {
+    public struct MigrationData
+    {
+        public string userName;
+        public string organization;
+        public string study;
+    }
+
     public static class LogFilesTos3
     {
         /// <summary>
         /// Move all files to s3 and on success move them from the staging area
         /// to the permanent area of logs
         /// </summary>
-        public static void MigrateFiles(string organization, string study, string game, string apiKey)
+        public static void MigrateFiles(IEnumerable<MigrationData> userData, string game, string apiKey)
         {
-            Assert.IsFalse(string.IsNullOrEmpty(organization));
-            Assert.IsFalse(string.IsNullOrEmpty(study));
             Assert.IsFalse(string.IsNullOrEmpty(game));
+            Assert.IsFalse(string.IsNullOrEmpty(apiKey));
 
-            string[] users = Directory.GetDirectories(LogDirectories.StagingDirectory);
-
-            for (int i = 0; i < users.Length; ++i)
+            foreach (MigrationData data in userData)
             {
-                MigrateUser(Path.GetFileName(users[i]), organization, study, game, apiKey);
+                MigrateUser(data, game, apiKey);
             }
         }
 
@@ -31,16 +36,14 @@ namespace BGC.Utility
         /// Migrate user logs from staging to permanent on succesful upload to s3
         /// </summary>
         private static void MigrateUser(
-            string userName,
-            string organization,
-            string study,
+            MigrationData migrationData,
             string game,
             string apiKey)
         {
 #if !UNITY_EDITOR
-            string permanentPath = LogDirectories.UserPermanentDirectory(userName);
-            string stagingPath = LogDirectories.UserStagingDirectory(userName);
-            string errorPath = LogDirectories.UserErrorLogDirectory(userName);
+            string permanentPath = LogDirectories.UserPermanentDirectory(migrationData.userName);
+            string stagingPath = LogDirectories.UserStagingDirectory(migrationData.userName);
+            string errorPath = LogDirectories.UserErrorLogDirectory(migrationData.userName);
             string[] files = Directory.GetFiles(stagingPath);
 
             for (int i = 0; i < files.Length; ++i)
@@ -53,11 +56,12 @@ namespace BGC.Utility
 
                 AWSServer.PostBGCToJSonToAWS(
                     stagingFile,
-                    organization,
-                    study, 
+                    migrationData.organization,
+                    migrationData.study,
                     game,
                     apiKey,
-                    (UnityWebRequest request, bool validJson) => {
+                    (UnityWebRequest request, bool validJson) =>
+                    {
                         if (validJson == true)
                         {
                             if (request.responseCode == 200)
@@ -73,6 +77,51 @@ namespace BGC.Utility
                         {
                             Debug.LogError($"Invalid json file: {stagingFile}");
                             IO.Utility.SafeMove(stagingFile, Path.Combine(errorPath, Path.GetFileName(stagingFile)));
+                        }
+                    });
+            }
+#endif
+        }
+
+        /// <summary>
+        /// Reupload user logs from permanent
+        /// </summary>
+        public static void ReUploadUser(
+            MigrationData migrationData,
+            string game,
+            string apiKey)
+        {
+#if !UNITY_EDITOR
+            string permanentPath = LogDirectories.UserPermanentDirectory(migrationData.userName);
+            string errorPath = LogDirectories.UserErrorLogDirectory(migrationData.userName);
+            string[] files = Directory.GetFiles(permanentPath);
+
+            for (int i = 0; i < files.Length; ++i)
+            {
+                string file = Path.Combine(permanentPath, files[i]);
+                if (ReservedFiles.IsFileReserved(file))
+                {
+                    continue;
+                }
+
+                AWSServer.PostBGCToJSonToAWS(
+                    file,
+                    migrationData.organization,
+                    migrationData.study,
+                    game,
+                    apiKey,
+                    (UnityWebRequest request, bool validJson) =>
+                    {
+                        if (validJson)
+                        {
+                            if (request.responseCode != 200)
+                            {
+                                Debug.LogError(request);
+                            }
+                        }
+                        else
+                        {
+                            Debug.LogError($"Invalid json file: {file}");
                         }
                     });
             }
