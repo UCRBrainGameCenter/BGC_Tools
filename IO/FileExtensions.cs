3df832ae8550f3b1fe5b3a7fad9ffe7d40672c79
TStavrop <Trevor.Stavropoulos@gmail.com>
2018-11-15 14:14:18 -0800

    Removing a highly unnecessary kind of silly extra function call from the FileExtensions class

--- IO/FileExtensions.cs
+++ IO/FileExtensions.cs
@@ -3,93 +3,48 @@
     public static class FileExtensions
     {
         public const string JSON = ".json";
-        public const string CSV  = ".csv";
-        public const string XML  = ".xml";
-        public const string BGC  = ".bgc";
+        public const string CSV = ".csv";
+        public const string XML = ".xml";
+        public const string BGC = ".bgc";
 
         /// <summary>
         /// Add json extension to string
         /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static string AddJsonExtension(string str)
-        {
-            return addExtension(str, JSON);
-        }
+        public static string AddJsonExtension(string str) => $"{str}{JSON}";
 
         /// <summary>
         /// Add csv extension to string
         /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static string AddCsvExtension(string str)
-        {
-            return addExtension(str, CSV);
-        }
+        public static string AddCsvExtension(string str) => $"{str}{CSV}";
 
         /// <summary>
         /// Add xml extension to string
         /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static string AddXmlExtension(string str)
-        {
-            return addExtension(str, XML);
-        }
+        public static string AddXmlExtension(string str) => $"{str}{XML}";
 
         /// <summary>
         /// Add bgc extension to string
         /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static string AddBgcExtension(string str)
-        {
-            return addExtension(str, BGC);
-        }
+        public static string AddBgcExtension(string str) => $"{str}{BGC}";
 
         /// <summary>
         /// Return true if the string ends with the json extension
         /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static bool HasJsonExtension(string str)
-        {
-            return str.EndsWith(JSON);
-        }
+        public static bool HasJsonExtension(string str) => str.EndsWith(JSON);
 
         /// <summary>
         /// Return true if the string ends with the csv extension
         /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static bool HasCsvExtension(string str)
-        {
-            return str.EndsWith(CSV);
-        }
+        public static bool HasCsvExtension(string str) => str.EndsWith(CSV);
 
         /// <summary>
         /// Return true if the string ends with the xml extension
         /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static bool HasXmlExtension(string str)
-        {
-            return str.EndsWith(XML);
-        }
+        public static bool HasXmlExtension(string str) => str.EndsWith(XML);
 
         /// <summary>
         /// Return true if the string ends with the bgc extension
         /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public static bool HasBgcExtension(string str)
-        {
-            return str.EndsWith(BGC);
-        }
-
-        private static string addExtension(string str, string extension)
-        {
-            return str + extension;
-        }
+        public static bool HasBgcExtension(string str) => str.EndsWith(BGC);
     }
 }
