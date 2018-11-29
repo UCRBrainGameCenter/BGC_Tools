e6c32a03a61fdf97a13350ef897d36d30cfd2140
Colan Biemer <colan@comcast.net>
2018-11-29 12:44:06 -0800

    removed stopwatch and cleaned up id manager a tad

--- Utility/IdManager.cs
+++ Utility/IdManager.cs
@@ -4,11 +4,36 @@
     {
         private int count = -1;
 
+        /// <summary>
+        /// Empty construct that starts ids at 0
+        /// </summary>
+        public IdManager() { }
+
+        /// <summary>
+        /// Construc id manager where the ids can either start at 0 or at the lowest
+        /// possible integer value
+        /// </summary>
+        /// <param name="humanFriendlyIds"></param>
+        public IdManager(bool humanFriendlyIds)
+        {
+            if (humanFriendlyIds == false)
+            {
+                count = int.MinValue;
+            }
+        }
+
+        /// <summary>
+        /// Reset starting id to 0
+        /// </summary>
         public void Reset()
         {
             count = -1;
         }
 
+        /// <summary>
+        /// Get an unused id
+        /// </summary>
+        /// <returns></returns>
         public int GetId()
         {
             ++count;
