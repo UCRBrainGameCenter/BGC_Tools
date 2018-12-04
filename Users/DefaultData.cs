964148de0d3bc2feee0b545f19eae4a7d00cdc95
TStavrop <Trevor.Stavropoulos@gmail.com>
2018-12-04 10:51:53 -0800

    Adding Users to BGC Tools.  StaticData is now PlayerData

---
+++ Users/DefaultData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace BGC.Users
+{
+    /// <summary>
+    /// Class to handle the default playerdata that is used when a player isn't logged in.
+    /// </summary>
+    public class DefaultData : ProfileData
+    {
+        public override bool IsDefault => true;
+
+        public DefaultData()
+            : base("Default")
+        {
+            if (File.Exists(PlayerFilePath))
+            {
+                Deserialize();
+            }
+            else
+            {
+                //Create the data
+                Serialize();
+            }
+        }
+
+        public override void DeletePlayerData()
+        {
+            throw new System.InvalidOperationException("Cannot delete default data files.");
+        }
+    }
+}
---
+++ Users/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace BGC.Users
+{
+    public class UserData : ProfileData
+    {
+        public override bool IsDefault => false;
+
+        //Constructor
+        public UserData(string userName)
+            : base(userName)
+        {
+        }
+
+    }
+}
+
