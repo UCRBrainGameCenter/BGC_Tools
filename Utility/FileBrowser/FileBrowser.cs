de9a81ff39f0c4514f85a3fd48edc68a4a9e5040
Colan Biemer <colan@comcast.net>
2019-04-15 14:08:34 -0700

    file browser ordering - UI added for ordering option - Directory and file display is now order by ascending or descending

--- Utility/FileBrowser/FileBrowser.cs
+++ Utility/FileBrowser/FileBrowser.cs
@@ -31,6 +31,8 @@ namespace BGC.Utility.FileBrowser
         private Button mainMenuButton = null;
         [SerializeField]
         private Button buttonPrefab = null;
+        [SerializeField]
+        private Button asccendingOrDescendingButton = null;
 
         [SerializeField]
         private Text pathText = null;
@@ -54,8 +56,12 @@ namespace BGC.Utility.FileBrowser
             ".txt", ".json", ".bgc", ".int", ".float", ".str", ".user"
         };
 
+        private Text ascendingOrderButtonText = null;
+        private bool ascendingOrder = false;
+
         private void Awake()
         {
+            Assert.IsNotNull(asccendingOrDescendingButton);
             Assert.IsNotNull(toggleNavigationButton);
             Assert.IsNotNull(parentDirectoryButton);
             Assert.IsNotNull(navigationContent);
@@ -67,8 +73,12 @@ namespace BGC.Utility.FileBrowser
             Assert.IsNotNull(pathText);
             Assert.IsNotNull(fileName);
 
-            parentDirectoryButton.onClick.AddListener(MoveUpDirectory);
+            ascendingOrderButtonText = asccendingOrDescendingButton.GetComponentInChildren<Text>();
+            Assert.IsNotNull(ascendingOrderButtonText);
+
+            asccendingOrDescendingButton.onClick.AddListener(ToggleDisplayOrder);
             toggleNavigationButton.onClick.AddListener(ToggleNavPanel);
+            parentDirectoryButton.onClick.AddListener(MoveUpDirectory);
             mainMenuButton.onClick.AddListener(GoBack);
 
             OpenDirectory(IO.DataManagement.RootDirectory);
@@ -83,6 +93,22 @@ namespace BGC.Utility.FileBrowser
         {
             Assert.IsFalse(string.IsNullOrEmpty(ReturnToScene));
             SceneManager.LoadScene(ReturnToScene);
+        }
+
+        private void ToggleDisplayOrder()
+        {
+            ascendingOrder = !ascendingOrder;
+
+            if (ascendingOrder)
+            {
+                ascendingOrderButtonText.text = "ASC";
+            }
+            else
+            {
+                ascendingOrderButtonText.text = "DESC";
+            }
+
+            OpenDirectory(currentDirectory);
         }
 
         private void ToggleNavPanel()
@@ -104,6 +130,15 @@ namespace BGC.Utility.FileBrowser
             childFiles.Clear();
 
             childDirectories = Directory.GetDirectories(currentDirectory).ToList();
+            if (ascendingOrder)
+            {
+                childDirectories.Sort((a, b) => -1 * a.CompareTo(b));
+            }
+            else
+            {
+                childDirectories.Sort();
+            }
+
             for (int i = 0; i < childDirectories.Count; i++)
             {
                 Button dir = Instantiate(buttonPrefab, Vector3.zero, Quaternion.identity, navigationContent.transform);
@@ -113,6 +148,15 @@ namespace BGC.Utility.FileBrowser
             }
 
             childFiles.AddRange(Directory.GetFiles(currentDirectory));
+            if (ascendingOrder)
+            {
+                childFiles.Sort((a, b) => -1 * a.CompareTo(b));
+            }
+            else
+            {
+                childFiles.Sort();
+            }
+
             for (int i = 0; i < childFiles.Count; i++)
             {
                 Button file = Instantiate(buttonPrefab, Vector3.zero, Quaternion.identity, navigationContent.transform);
@@ -136,7 +180,7 @@ namespace BGC.Utility.FileBrowser
             int children = obj.transform.childCount;
             for (int i = children - 1; i >= 0; i--)
             {
-                Destroy(obj.transform.GetChild(i).gameObject); //Destroy the child.
+                Destroy(obj.transform.GetChild(i).gameObject);
             }
         }
 
@@ -144,7 +188,11 @@ namespace BGC.Utility.FileBrowser
         {
             currentDirectory = path;
             UpdateChildren();
-            UpdatePathText();
+
+            // set UI for directory path
+            string directory = currentDirectory.Remove(0, IO.DataManagement.RootDirectory.Length);
+            directory = directory.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+            pathText.text = $"Path: {directory}/";
         }
 
         private void OpenFile(string path)
@@ -159,13 +207,6 @@ namespace BGC.Utility.FileBrowser
             }
 
             fileName.text = $"({Path.GetFileName(path)})";
-        }
-
-        private void UpdatePathText()
-        {
-            string directory = currentDirectory.Remove(0, IO.DataManagement.RootDirectory.Length);
-            directory = directory.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
-            pathText.text = $"Path: {directory}/";
         }
     }
 }
