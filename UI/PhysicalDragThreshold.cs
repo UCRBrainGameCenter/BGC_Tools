cf73bc47c3091d163a9cdeaab1ebb4d58f7f3919
Trevor Stavropoulos <Trevor.Stavropoulos@gmail.com>
2019-11-26 10:51:13 -0800

    Adding Monobehavior to adjust dragthreshold based on DPI

---
+++ UI/PhysicalDragThreshold.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BGC.UI
+{
+    /// <summary>
+    /// Sets the drag threshold for an EventSystem as a physical size based on DPI.
+    /// Taken from:
+    /// https://forum.unity.com/threads/buttons-within-scroll-rect-are-difficult-to-press-on-mobile.265682/#post-1993502
+    /// </summary>
+    [RequireComponent(typeof(EventSystem))]
+    public class PhysicalDragThreshold : MonoBehaviour
+    {
+        [SerializeField]
+        private float dragThresholdInches = 0.125f;
+
+        void Start()
+        {
+            GetComponent<EventSystem>().pixelDragThreshold = (int)(dragThresholdInches * Screen.dpi);
+        }
+    }
+}
