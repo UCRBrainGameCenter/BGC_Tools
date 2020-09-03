551f903c862e05fbb4375bc8f9d072f2ce733f8f
David Clamage <rangsk@gmail.com>
2020-09-03 12:02:01 -0700

    Added ToSingleItemEnumberable extension.

--- Extensions/Enumerable.cs
+++ Extensions/Enumerable.cs
@@ -11,6 +11,10 @@ namespace BGC.Extensions.Linq
             (float)Math.Pow(source.Aggregate(1f, ProductAccumulate), 1.0 / source.Count());
         public static double GeometricMean(this IEnumerable<double> source) =>
             Math.Pow(source.Aggregate(1.0, ProductAccumulate), 1.0 / source.Count());
+        public static IEnumerable<T> ToSingleItemEnumberable<T>(this T item)
+        {
+            yield return item;
+        }
 
         public static float ProductAccumulate(float total, float value) => total * value;
         public static double ProductAccumulate(double total, double value) => total * value;
