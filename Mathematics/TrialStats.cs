5eea5de5b969b4644cb7e4856f4f759ba0c98f7d
David Clamage <dclamage@users.noreply.github.com>
2022-11-08 01:22:11 -0800

    TrialStats: Reaction time for misses/total

--- Mathematics/TrialStats.cs
+++ Mathematics/TrialStats.cs
@@ -10,9 +10,13 @@ namespace BGC.Mathematics
         public int NoResponses { get; private set; } = 0;
         public int Trials => Hits + Misses + NoResponses;
         public double HitAccuracy => GeneralMath.Accuracy(Hits, Trials);
-        public double HitAverageReactionTime => reactionTimeAccum.Mean;
+        public double AverageReactionTime => reactionTimeAccum.Mean;
+        public double HitAverageReactionTime => hitReactionTimeAccum.Mean;
+        public double MissAverageReactionTime => missReactionTimeAccum.Mean;
 
         private StatsAccumulator reactionTimeAccum = new StatsAccumulator();
+        private StatsAccumulator hitReactionTimeAccum = new StatsAccumulator();
+        private StatsAccumulator missReactionTimeAccum = new StatsAccumulator();
 
         public TrialStats(string name)
         {
@@ -25,13 +29,24 @@ namespace BGC.Mathematics
             Misses = 0;
             NoResponses = 0;
             reactionTimeAccum = new StatsAccumulator();
+            hitReactionTimeAccum = new StatsAccumulator();
+            missReactionTimeAccum = new StatsAccumulator();
         }
+
         public void Hit(double reactionTime)
         {
             Hits++;
             reactionTimeAccum.Append(reactionTime);
+            hitReactionTimeAccum.Append(reactionTime);
         }
-        public void Miss() => Misses++;
+
+        public void Miss(double reactionTime)
+        {
+            Misses++;
+            reactionTimeAccum.Append(reactionTime);
+            missReactionTimeAccum.Append(reactionTime);
+        }
+
         public void NoResponse() => NoResponses++;
 
         public void AddSummary(JsonObject obj)
@@ -40,7 +55,9 @@ namespace BGC.Mathematics
             obj.Add($"{Name}Misses", Misses);
             obj.Add($"{Name}NoResponses", NoResponses);
             obj.Add($"{Name}Accuracy", HitAccuracy);
+            obj.Add($"{Name}AverageReactionTime", AverageReactionTime);
             obj.Add($"{Name}HitAverageReactionTime", HitAverageReactionTime);
+            obj.Add($"{Name}MissAverageReactionTime", MissAverageReactionTime);
         }
     }
 }
