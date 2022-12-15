99f0b910bcd7b84c27c81b70f74a004d2d4635a6
David Clamage <dclamage@users.noreply.github.com>
2022-12-14 16:45:35 -0800

    Added adaptive difficulty algorithms (from Corsi) and unit tests for them.

---
+++ AdaptiveDifficultyAlgorithm/IAdaptiveDifficultyAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using BGC.Parameters;
+
+namespace BGC.AdaptiveDifficultyAlgorithm
+{
+    [PropertyGroupTitle("Adaptive Difficulty Algorithm")]
+    public interface IAdaptiveDifficultyAlgorithm : IPropertyGroup
+    {
+        int Difficulty { get; }
+        bool IsDone { get; }
+        int Threshold { get; }
+
+        void Initialize();
+        bool SubmitTrialResult(bool correct);
+    }
+}
