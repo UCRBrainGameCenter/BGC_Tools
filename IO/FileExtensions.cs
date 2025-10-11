e82f16b3648666e540e2708c36f34f670553db71
David Clamage <rangsk@gmail.com>
2025-10-10 21:11:22 -0600

    Renamed JSONS to JSONL to match the standard format

--- IO/FileExtensions.cs
+++ IO/FileExtensions.cs
@@ -6,7 +6,7 @@
         public const string CSV = ".csv";
         public const string XML = ".xml";
         public const string BGC = ".bgc";
-        public const string JSONS = ".jsons";
+        public const string JSONL = ".jsonl";
 
         /// <summary>
         /// Add json extension to string
@@ -49,8 +49,8 @@
         public static bool HasBgcExtension(string str) => str.EndsWith(BGC);
 
         /// <summary>
-        /// Return true if the string ends with the jsons extension
+        /// Return true if the string ends with the jsonl extension
         /// </summary>
-        public static bool HasJsonsExtension(string str) => str.EndsWith(JSONS);
+        public static bool HasJsonlExtension(string str) => str.EndsWith(JSONL);
     }
 }
